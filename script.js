51db2c407cb5620ad8e292eb6ca84353b49122b1
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-03-27 15:35:00 +0200

    altra prova per fix vh mobile

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', function() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+});
